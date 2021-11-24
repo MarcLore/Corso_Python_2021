51550a112938c99502f11aa9e17f9e336e3715dd
MarcLore <lorenzo.marchegiani97@gmail.com>
2021-11-24 18:18:36 +0100

    Esercizi

---
+++ Lezione 7/Esercizio06.py
@@ -0,0 +1,7 @@
+#Scrivere un programma Python per invertire una tupla di elementi presi in input
+
+def Esercizio_Tupla03():
+    tuplex = ("a", "b", "c")
+    print(tuplex[::-1])
+
+Esercizio_Tupla03()
